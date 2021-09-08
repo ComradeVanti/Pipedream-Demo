795ce6694d61c2052bb499ba7d48f2edbeaa511e
KingVanti <kingvanti650@gmail.com>
2021-09-08 09:30:45 +0200

    Add logic for editing inputs
    Inputs can be edited by the user. The user may only enter numbers. Resolves #5

--- PipedreamDemo.Browser/src/Editor.fs
+++ PipedreamDemo.Browser/src/Editor.fs
@@ -5,25 +5,37 @@ open Elmish
 open Feliz
 open PipedreamDemo
 
+type InputIndex = int
+
 type State = { Inputs: Input list }
 
 [<RequireQualifiedAccess>]
-type Msg = unit
+type Msg = InputChanged of InputIndex * float
 
 let init _ : State * Cmd<Msg> =
     { Inputs = [ InputValue 0.; InputValue 0. ] }, Cmd.none
 
-let update msg state : State * Cmd<Msg> = state, Cmd.none
+let update msg state =
+    match msg with
+    | Msg.InputChanged (index, value) ->
+        { state with
+            Inputs = state.Inputs |> replaceAtIndex (InputValue value) index
+        },
+        Cmd.none
 
-let private viewInput (InputValue value) dispatch =
-    Html.div [ prop.className "input"; prop.text value ]
+let private viewInput (InputValue value) index dispatch =
+    Html.input [ prop.className "input"
+                 prop.value value
+                 prop.type' "number"
+                 prop.onChange (fun v -> dispatch (Msg.InputChanged(index, v))) ]
 
 let private viewInputs inputs dispatch =
     Html.div [ prop.id "inputs"
                prop.children (
                    List.append
                        [ Html.h2 [ prop.text "Inputs" ] ]
-                       (inputs |> viewAllBy viewInput dispatch)
+                       (inputs
+                        |> List.mapi (fun i input -> viewInput input i dispatch))
                ) ]
 
 let view state dispatch =
---
+++ PipedreamDemo.Core/ListUtil.fs
@@ -0,0 +1,9 @@
+﻿[<AutoOpen>]
+module PipedreamDemo.ListUtil
+
+let mapAtIndex mapper index list =
+    list
+    |> List.mapi (fun i item -> if i = index then mapper item else item)
+
+let replaceAtIndex newItem index list =
+    list |> mapAtIndex (fun _ -> newItem) index
