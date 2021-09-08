1eedee47af8882edd80cdd66b0de4adeea8f77a4
KingVanti <kingvanti650@gmail.com>
2021-09-08 07:45:55 +0200

    Add elmish editor component

---
+++ PipedreamDemo.Browser/src/Editor.fs
@@ -0,0 +1,17 @@
+﻿[<RequireQualifiedAccess>]
+module PipedreamDemo.Browser.Editor
+
+open Elmish
+open Feliz
+
+[<RequireQualifiedAccess>]
+type State = unit
+
+[<RequireQualifiedAccess>]
+type Msg = unit
+
+let init _ : State * Cmd<Msg> = (), Cmd.none
+
+let update msg state : State * Cmd<Msg> = state, Cmd.none
+
+let view dispatch state = Html.div [ prop.id "editor" ]
