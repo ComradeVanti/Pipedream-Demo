81ae000c770f4a1a8f48f7a7f7a511bf8d9ecac0
KingVanti <kingvanti650@gmail.com>
2021-09-08 08:14:57 +0200

    Add input type and integrate into program
    Add input type, which just contains a float and add it to the editor state

--- PipedreamDemo.Browser/src/Editor.fs
+++ PipedreamDemo.Browser/src/Editor.fs
@@ -3,14 +3,15 @@ module PipedreamDemo.Browser.Editor
 
 open Elmish
 open Feliz
+open PipedreamDemo
 
-[<RequireQualifiedAccess>]
-type State = unit
+type State = { Inputs: Input list }
 
 [<RequireQualifiedAccess>]
 type Msg = unit
 
-let init _ : State * Cmd<Msg> = (), Cmd.none
+let init _ : State * Cmd<Msg> =
+    { Inputs = [ InputValue 0.; InputValue 0. ] }, Cmd.none
 
 let update msg state : State * Cmd<Msg> = state, Cmd.none
 
